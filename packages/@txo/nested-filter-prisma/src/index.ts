bd345b2146ef900a2ba21ad68edf3668b633ac4e
Erik Slovák <34162967+erik-slovak@users.noreply.github.com>
2023-11-22 08:58:23 +0100

    chore: migrate package (#2)
    * chore: migrate package
    
    * chore: remove old files
    
    * chore: remove lodash types

--- packages/@txo/nested-filter-prisma/src/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @Author: Rostislav Simonik <rostislav.simonik@technologystudio.sk>
- * @Date: 2021-04-18T10:04:54+02:00
- * @Copyright: Technology Studio
-**/
-
-export * from './Api'
-export * from './Model/Types'
-export * from './Middleware'
